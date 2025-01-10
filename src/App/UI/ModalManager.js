46fb18bcb71f9b93e45b7e9372e3272f8be0c8f8
Hamilton <>
2025-01-10 16:12:55 +0100

    Refactor + linting

--- src/App/UI/ModalManager.js
+++ src/App/UI/ModalManager.js
@@ -1,38 +1,37 @@
 export default class ModalManager {
   constructor() {
-    this.modal = document.getElementById("modal");
-    this.close = document.getElementsByClassName("close")[0];
-    this.description = document.getElementById("modalDescription");
-    this.img = document.getElementById("modalImg");
-    this.restartCallback = null; // Add a property to store the restart callback
+    this.modal = document.getElementById('modal')
+    this.close = document.getElementsByClassName('close')[0]
+    this.description = document.getElementById('modalDescription')
+    this.img = document.getElementById('modalImg')
+    this.restartCallback = null // Add a property to store the restart callback
 
     this.close.onclick = () => {
       if (this.restartCallback) {
-        this.restartCallback();
-        this.restartCallback = null;
+        this.restartCallback()
+        this.restartCallback = null
       }
-      this.closeModal();
-    };
+      this.closeModal()
+    }
   }
 
   openModal(title, description, imgPath, typeDeath, restartCallback) {
-    document.getElementById("modalTitle").innerHTML = title;
-    description
-      ? (this.description.innerHTML = description)
-      : (this.description.innerHTML = null);
-    imgPath ? (this.img.src = imgPath) : (this.img.src = null);
+    document.getElementById('modalTitle').innerHTML = title
+    this.description.innerHTML = description || ''
+    this.img.src = imgPath || ''
+    
     if (typeDeath && restartCallback) {
-      this.close.innerHTML = "Restart";
-      this.restartCallback = restartCallback; // Store the callback
+      this.close.innerHTML = 'Restart'
+      this.restartCallback = restartCallback // Store the callback
     } else {
-      this.close.innerHTML = "Close";
-      this.restartCallback = null; // Clear the callback if not applicable
+      this.close.innerHTML = 'Close'
+      this.restartCallback = null // Clear the callback if not applicable
     }
 
-    this.modal.classList.add("visible");
+    this.modal.classList.add('visible')
   }
 
   closeModal() {
-    this.modal.classList.remove("visible");
+    this.modal.classList.remove('visible')
   }
 }
