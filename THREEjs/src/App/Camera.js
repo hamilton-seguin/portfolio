b9718db60551b872163c34cea5d70807b88a928d
Hamilton <>
2025-01-12 15:34:25 +0100

    Rename app in package.json

--- THREEjs/src/App/Camera.js
+++ THREEjs/src/App/Camera.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { sizesStore } from './Utils/Store.js'
 
 import App from './App.js'
