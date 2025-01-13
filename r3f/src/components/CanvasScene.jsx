81764969ad046f2d3c3251ad590e8b27d694e5bf
Hamilton <>
2025-01-13 15:39:20 +0100

    Update CanvasScene to include selected Character model

--- r3f/src/components/CanvasScene.jsx
+++ r3f/src/components/CanvasScene.jsx
@@ -2,18 +2,29 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 
-import Lights from '@/components/Lights'
+import { CharacterWireframe, Character } from '@/components/Character'
+import { Lights } from '@/components/Lights'
+// import { Camera } from '@/components/Camera'
 
-import { LogMesh } from '@/utils'
+import { appStateStore } from '@/utils/store'
+import { LogMesh, LogScene } from '@/utils'
 
-const CanvasScene = () => (
-  <Canvas>
-    <Lights />
+export default function CanvasScene() {
+  const characterSelected = appStateStore((state) => state.characterSelected)
 
-    <Character />
-    <AnimatedCharacter 
-      position={[0, 10, 0]}/>
-  </Canvas>
-)
+  return (
+    <Canvas>
+      <Lights />
+      {/* <Camera /> */}
 
-export default CanvasScene
+      <CharacterWireframe />
+      {characterSelected && (
+        <Character position={[0, 10, 0]} avatarChosen={characterSelected} />
+      )}
+
+      <OrbitControls enableDamping />
+      <LogScene />
+      {/* <LogMesh meshName={'floorTarget'} /> */}
+    </Canvas>
+  )
+}
