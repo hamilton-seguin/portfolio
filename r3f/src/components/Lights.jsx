c0f8424e3fc09fa35d2d43b5d7000416467fa158
Hamilton <>
2025-01-15 17:43:54 +0100

    Fix lights

--- r3f/src/components/Lights.jsx
+++ r3f/src/components/Lights.jsx
@@ -17,24 +17,26 @@ export const Lights = () => {
       <directionalLight
         ref={directionalLightRef}
         position={[34, 50, -29]}
-        color={0xe39230}
+        color={0xd1ad69}
         intensity={0.28}
         castShadow
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
         shadow-bias={-0.0001}
-        shadow-normalBias={-0.01}
+        shadow-normalBias={-0.001}
+        
       >
-        <orthographicCamera attach="shadow-camera" args={[-40, 40, 30, -30]}/>
+        <orthographicCamera attach="shadow-camera" args={[-40, 40, 30, -30]} />
       </directionalLight>
       <directionalLight
         position={[-19, 15, -38]}
         color={0x35359c}
-        intensity={0.1}
-        // castShadow
+        intensity={0.06}
         shadow-camera-top={3}
         shadow-camera-right={3}
         shadow-camera-bottom={-3}
         shadow-camera-left={-3}
-        />
+      />
     </>
   )
 }
