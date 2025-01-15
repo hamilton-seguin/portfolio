a31e223f4e10703388b25a731c680b31a4dcf689
Hamilton <>
2025-01-15 17:44:17 +0100

    Update Canvas with more settings

--- r3f/src/components/CanvasScene.jsx
+++ r3f/src/components/CanvasScene.jsx
@@ -1,10 +1,12 @@
 'use client'
 import { Canvas } from '@react-three/fiber'
+import { Physics } from '@react-three/rapier'
 import { OrbitControls } from '@react-three/drei'
 
-import { CharacterWireframe, Character } from '@/components/Character'
+import { Character } from '@/components/Character'
 import { Lights } from '@/components/Lights'
-// import { Camera } from '@/components/Camera'
+import { Environment } from '@/components/Environment'
+import { Controller } from '@/components/Controller'
 
 import { appStateStore } from '@/utils/store'
 import { LogMesh, LogScene } from '@/utils'
@@ -13,18 +15,35 @@ export default function CanvasScene() {
   const characterSelected = appStateStore((state) => state.characterSelected)
 
   return (
-    <Canvas>
-      <Lights />
-      {/* <Camera /> */}
+    <div className="w-screen h-screen">
+      <Canvas
+        shadows
+        camera={{ fov: 35 }}
+        gl={{
+          toneMapping: 1,
+          toneMappingExposure: 5,
+        }}
+        onCreated={({ gl }) => {
+          console.log('gl', gl)
+        }}
+        onPointerDown={(e) => e.target.requestPointerLock()}
+        fallback={<div>Sorry WebGL is not supported by your device!</div>}
+      >
+        <Lights />
 
-      <CharacterWireframe />
-      {characterSelected && (
-        <Character position={[0, 10, 0]} avatarChosen={characterSelected} />
-      )}
+        <Physics timeStep="vary">
+          <Environment />
+          {characterSelected && (
+            <Controller>
+              <Character avatarChosen={characterSelected} />
+            </Controller>
+          )}
+        </Physics>
 
-      <OrbitControls enableDamping />
-      <LogScene />
-      {/* <LogMesh meshName={'floorTarget'} /> */}
-    </Canvas>
+        <OrbitControls enableDamping />
+        <LogScene />
+        {/* <LogMesh meshName={'floorTarget'} /> */}
+      </Canvas>
+    </div>
   )
 }
