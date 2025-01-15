d55b1ea8693552b0c21a9f68f6ecc3b87dcd49e9
Hamilton <>
2025-01-15 18:47:11 +0100

    Remove unused things

--- r3f/src/components/Character.jsx
+++ r3f/src/components/Character.jsx
@@ -1,31 +1,14 @@
-import { useEffect, useRef } from 'react'
-import { useGLTF, useAnimations } from '@react-three/drei'
-
-export const CharacterWireframe = () => {
-  return (
-    <mesh position={[0, 10, 0]} name="CharacterWireframe">
-      <boxGeometry args={[0.6, 2, 0.6]} />
-      <meshStandardMaterial color={0x00ff00} wireframe visible={true} />
-    </mesh>
-  )
-}
+import { Gltf } from '@react-three/drei'
 
 export function Character({ avatarChosen = null }) {
-  const { scene, animations } = useGLTF(`/models/${avatarChosen}.glb`, '/draco-gltf/')
-  const { actions } = useAnimations(animations, scene)
-  const avatarRef = useRef()
-
-  useEffect(() => {
-    actions?.[animations[1].name]?.play()
-  }, [actions, animations])
-
   return (
-    <primitive
-      ref={avatarRef}
-      object={scene}
-      scale={[1.1, 1.1, 1.1]}
-      rotation-y={Math.PI}
-      position={[0, 9, 0]}
+    <Gltf
+      scale={0.8}
+      position={[0, -1, 0]}
+      castShadow
+      receiveShadow
+      src={`/models/${avatarChosen}.glb`}
+      useDraco="/draco-gltf"
     />
   )
 }
