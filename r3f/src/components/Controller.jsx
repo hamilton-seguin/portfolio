75f93ea50264f8814e75cd1c34af684e882d023e
Hamilton <>
2025-01-15 17:43:38 +0100

    Fix Controller: camera, character physics

--- r3f/src/components/Controller.jsx
+++ r3f/src/components/Controller.jsx
@@ -14,7 +14,28 @@ export const Controller = ({ children }) => {
 
   return (
     <KeyboardControls map={keyboardMap}>
-      <CharacterController maxVelLimit={5} position={[7.8, 4, 10]} >{children}</CharacterController>
+      <CharacterController
+        maxVelLimit={5}
+        position={[8.3, 4, 10.5]}
+        capsuleHalfHeight={0.45}
+        characterInitDir={16.3}
+        // mode={'FixedCamera'}
+        // fixedCamRotMult={2}
+        // camLerpMult={5}
+        // camInitDis={-7}
+        // camMaxDism={-20}
+        // camMinDis={3}
+        camInitDir={{ x: 0.32, y: 3.75 }}
+        camTargetPos={{ x: 0, y: 2.6, z: -7 }}
+
+        sprintMult={1.4}
+        jumpVel={3}
+        jumpForceToGroundMult={3.5}
+        moveImpulsePointY={0} //makes you bend sideways when moving
+        // floatHeight={0.1}
+      >
+        {children}
+      </CharacterController>
     </KeyboardControls>
   )
 }
