011ed238f5601b231190874d284c4cd9cf5bf1f6
Hamilton <>
2025-01-16 18:47:47 +0100

    Update Nasa page

--- r3f/src/app/nasa-api/page.jsx
+++ r3f/src/app/nasa-api/page.jsx
@@ -1,3 +1,16 @@
-export default function NasaApi() {
-  return ('TEST')
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Nasa API | Portfolio</title>
+        <meta name="description" content="Nasa API | Hamilton Seguin Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#050a14" />
+      </Head>
+
+      <h1>Nasa API</h1>
+    </>
+  )
 }
