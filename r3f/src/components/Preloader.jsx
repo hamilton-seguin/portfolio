b042620cd3e896c4d9e4499a413d9d789047a360
Hamilton <>
2025-01-16 18:19:11 +0100

    Add anim to Preloader and unmount it after fact

--- r3f/src/components/Preloader.jsx
+++ r3f/src/components/Preloader.jsx
@@ -1,34 +1,50 @@
 'use client'
 import { useGLTF } from '@react-three/drei'
+import { useEffect, useState } from 'react'
 
 import { AssetLoader } from '@/components/AssetLoader'
 
 import { appStateStore } from '@/utils/store'
 
 export const Preloader = () => {
+  const [characterChosen, setCharacterChosen] = useState('')
+  const [done, setDone] = useState(false)
   const assetsReady = appStateStore((state) => state.assetsReady)
-  const characterSelected = appStateStore((state) => state.characterSelected)
 
-  const oppositeCharacter = ['h-avatar', 'm-avatar'].filter(
-    (c) => c !== characterSelected
-  )[0]
-  const url = `/models/${oppositeCharacter}.glb`
-
-  const handleCharacterSelect = (characterId) => {
-    appStateStore.setState({
-      characterSelected: characterId,
-    })
-    useGLTF.clear(url)
+  const oppositeCharacter = (url) => {
+    return Array('h-avatar', 'm-avatar').filter((c) => c !== url)[0]
   }
 
-  if (characterSelected) return null
+  useEffect(() => {
+    let timer
+    if (characterChosen) {
+      timer = window.setTimeout(() => {
+        setDone(true)
+      }, 1500)
+    }
+    return () => clearTimeout(timer)
+  }, [characterChosen])
+
+  const handleCharacterSelect = (characterId) => {
+    setCharacterChosen(characterId)
+    appStateStore.setState({ characterSelected: characterId })
+    useGLTF.clear(`/models/${oppositeCharacter(characterId)}.glb`)
+    document.getElementById('preloader').classList.add('opacity-0')
+  }
+
+  if (done) return null
 
   return (
-    <div className="fixed bg-[#050a14] w-full h-full text-white z-10">
-      <div className="flex justify-center items-center h-full text-white transition-opacity duration-1000">
-        {!assetsReady && <AssetLoader />}
-
-        {!characterSelected && assetsReady && (
+    <div
+      id="preloader"
+      className={`fixed w-full h-full text-white z-10 transition-all duration-1000 ${
+        characterChosen ? 'opacity-0 bg-transparent' : 'opacity-100 bg-[#050a14]'
+      }`}
+    >
+      <div className="flex justify-center items-center h-full text-white">
+        {!assetsReady ? (
+          <AssetLoader />
+        ) : (
           <div className="flex gap-8">
             <button
               type="button"
