f175c299e5ba7accebd40816f57bed0c58e2ddd2
Hamilton <>
2025-01-16 18:19:38 +0100

    Lint

--- r3f/src/utils/index.jsx
+++ r3f/src/utils/index.jsx
@@ -4,22 +4,18 @@ import { Color } from 'three'
 
 export const LogMesh = ({ meshName }) => {
   const { scene } = useThree()
-
   useEffect(() => {
     const meshToLog = scene.getObjectByName(meshName)
     console.log(`Logging Mesh: '${meshName}'`, meshToLog)
   }, [scene])
-
   return null
 }
 
 export const LogScene = () => {
   const { scene } = useThree()
-
   useEffect(() => {
     console.log('Logging Scene: ', scene)
   }, [scene])
-
   return null
 }
 
