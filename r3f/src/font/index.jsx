9ba70cbadb5a4f9e0bc178c1ce791d413280e991
Hamilton <>
2025-01-16 23:11:08 +0100

    Change font to fira_mono

--- r3f/src/font/index.jsx
+++ r3f/src/font/index.jsx
@@ -1,23 +1,20 @@
-import localFont from 'next/font/local'
-import { Amatic_SC } from 'next/font/google'
-
-export const andaleMono = localFont({
-  src: [
-    {
-      path: './fonts/AndaleMono.woff2',
-      weight: 'normal',
-      style: 'normal',
-    },
-  ],
-  variable: '--font-andale',
-  fallback: ['monospace'],
-  preload: true,
-})
+import { Amatic_SC, Fira_Mono } from 'next/font/google'
 
 export const amatic = Amatic_SC({
   subsets: ['latin'],
   weight: ['400'],
   fallback: ['cursive'],
   variable: '--font-amatic',
+  display: 'swap',
+  preload: true,
+})
+
+export const fira = Fira_Mono({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  fallback: ['mono'],
+  style: 'normal',
+  variable: '--font-fira',
+  display: 'swap',
   preload: true,
 })
