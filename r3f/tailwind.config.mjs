6d591a3e1dbae809ffda2798a61d10fff9e62c24
Hamilton <>
2025-01-27 15:50:12 +0100

    Add extra tailwind screen size as xs for small devices

--- r3f/tailwind.config.mjs
+++ r3f/tailwind.config.mjs
@@ -11,6 +11,9 @@ const config = {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
       },
+      screens: {
+        xs: '400px',
+      },
     },
   },
   plugins: [],
