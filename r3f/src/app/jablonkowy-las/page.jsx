2d0bc9e9691d89d364b3df59da018565fb73cf3a
Hamilton <>
2025-01-27 15:50:31 +0100

    Create page for Jablonkowylas

---
+++ r3f/src/app/jablonkowy-las/page.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Jablonkowy Las | Portfolio</title>
+        <meta name="description" content="Jablonkowy Las | Hamilton Seguin Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#050a14" />
+      </Head>
+
+      <h1>Jablonkowy Las</h1>
+    </>
+  )
+}
