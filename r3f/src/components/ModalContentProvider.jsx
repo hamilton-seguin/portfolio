e3b98f505f800acfb76058371e7fd88bb14fc3a6
Hamilton <>
2025-01-31 12:39:06 +0100

    Fix css of Modals on all screen sizes

--- r3f/src/components/ModalContentProvider.jsx
+++ r3f/src/components/ModalContentProvider.jsx
@@ -8,7 +8,13 @@ const CustomLink = ({ url, children }) => {
   if (!url) return <>{children}</>
   if (url.includes('https')) {
     return (
-      <a href={url} target="_blank" noreferrer="true" noopener="true" className="underline">
+      <a
+        href={url}
+        target="_blank"
+        noreferrer="true"
+        noopener="true"
+        className="underline"
+      >
         {children}
       </a>
     )
@@ -60,71 +66,66 @@ export const ModalContentProvider = ({ portalName }) => {
       )}
     </div>
   ) : (
-    <div className="flex flex-col gap-6 items-center text-center relative min-w-[60vw] max-w-[80vw] mb-2">
+    <div className="flex flex-col gap-6 items-center text-center min-w-[60vw] max-w-[80vw]">
       <h1 className="font-bold text-3xl">Projects</h1>
-      <div className="relative w-full">
-        <div
-          key={projectsData[currentIndex].title}
-          className="flex flex-col gap-4 items-center"
-        >
-          <div>
-            <CustomLink url={projectsData[currentIndex].url}>
-              <h2 className="font-medium text-xl">
-                {projectsData[currentIndex].title}
-              </h2>
-            </CustomLink>
-            <h3 className="text-sm italic">
-              {projectsData[currentIndex].subtitle}
-            </h3>
-          </div>
-          <ul className="flex gap-2 flex-wrap justify-center">
-            {projectsData[currentIndex].stacks.map((stack, i) => (
-              <li key={i} className="text-sm">
-                {stack}
-              </li>
-            ))}
-          </ul>
-          <div className="flex gap-4 justify-center items-center w-full">
-            <button
-              onClick={handlePrev}
-              className="bg-slate-500/20 text-white px-3 py-1 sm:py-2 rounded-full text-3xl sm:text-5xl shadow-transparent/20 hover:text-[#9effe7] hover:bg-[#ffffff4d] transition-colors"
-            >
-              &lt;
-            </button>
-            <Image
-              src={projectsData[currentIndex].img}
-              width={400}
-              height={400}
-              alt={projectsData[currentIndex].title}
-              priority
-              className="max-w-full min-w-[60vw] sm:min-w-[30vw] mb-8"
-            />
+      <div className="relative w-full flex flex-col gap-4 items-center">
+        <div>
+          <CustomLink url={projectsData[currentIndex].url}>
+            <h2 className="font-medium text-xl">
+              {projectsData[currentIndex].title}
+            </h2>
+          </CustomLink>
+          <h3 className="text-sm italic">
+            {projectsData[currentIndex].subtitle}
+          </h3>
+        </div>
+        <ul className="flex gap-2 flex-wrap justify-center">
+          {projectsData[currentIndex].stacks.map((stack, i) => (
+            <li key={i} className="text-sm">
+              {stack}
+            </li>
+          ))}
+        </ul>
+        <div className="flex gap-4 justify-center items-center w-full mt-4 mb-8">
+          <button
+            onClick={handlePrev}
+            className="bg-slate-500/20 text-white px-3 py-1 sm:py-2 rounded-full text-3xl sm:text-5xl shadow-transparent/20 hover:text-[#9effe7] hover:bg-[#ffffff4d] transition-colors"
+          >
+            &lt;
+          </button>
+          <Image
+            src={projectsData[currentIndex].img}
+            width={400}
+            height={400}
+            alt={projectsData[currentIndex].title}
+            priority
+            className="max-w-full min-w-[60vw] sm:min-w-[30vw]"
+          />
 
-            <button
-              onClick={handleNext}
-              className="bg-slate-500/20 text-white px-3 py-1 sm:py-2 rounded-full text-3xl sm:text-5xl shadow-transparent/20 hover:text-[#9effe7] hover:bg-[#ffffff4d] transition-colors"
-            >
-              &gt;
-            </button>
-          </div>
+          <button
+            onClick={handleNext}
+            className="bg-slate-500/20 text-white px-3 py-1 sm:py-2 rounded-full text-3xl sm:text-5xl shadow-transparent/20 hover:text-[#9effe7] hover:bg-[#ffffff4d] transition-colors"
+          >
+            &gt;
+          </button>
+        </div>
 
-          <div className="flex flex-col gap-6 max-h-[30vh] xs:max-h-[40vh] sm:max-h-[50vh] lg:max-h-full overflow-y-auto justify-center items-center">
-            <p className="text-base leading-snug w-11/12 ">
-              {projectsData[currentIndex].description}
-            </p>
-            <div className="flex flex-col gap-2 w-10/12 justify-center items-center">
-              <b className="text-base leading-snug">Key Achievements</b>
-              <ul className="text-base leading-snug">
-                {projectsData[currentIndex].achievements.map((achievement) => {
-                  const [title, description] = achievement.split(': ')
-                  return (
-                    <li key={achievement}>
-                      <b>{title}:</b> {description}
-                    </li>
-                  )
-                })}
-              </ul>
-            </div>
+        <div className="flex flex-col gap-6 max-h-[30vh] xs:max-h-[40vh] sm:max-h-[42vh] overflow-y-auto justify-start items-center">
+          <p className="text-base leading-snug w-11/12 ">
+            {projectsData[currentIndex].description}
+          </p>
+          <div className="flex flex-col gap-2 w-10/12 justify-center items-center">
+            <b className="text-base leading-snug">Key Achievements</b>
+            <ul className="text-base leading-snug">
+              {projectsData[currentIndex].achievements.map((achievement) => {
+                const [title, description] = achievement.split(': ')
+                return (
+                  <li key={achievement}>
+                    <b>{title}:</b> {description}
+                  </li>
+                )
+              })}
+            </ul>
           </div>
         </div>
       </div>
